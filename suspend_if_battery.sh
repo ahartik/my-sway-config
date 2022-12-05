8fb544a4a3636b5d28ce588e9996d27fd2c91ee9
Aleksi Hartikainen <ahartik@gmail.com>
2022-12-05 20:13:06 +0200

    swayidle

--- suspend_if_battery.sh
+++ suspend_if_battery.sh
@@ -3,6 +3,7 @@ if grep -q Discharging /sys/class/power_supply/BAT0/status
 then
   echo "on battery, suspending"
   systemctl suspend
+  # swaymsg "output * dpms on"
 else
   echo "on DC, not suspending"
 fi
--- swayidle.sh
+++ swayidle.sh
@@ -2,6 +2,7 @@
 
 swayidle timeout 310 "~/.config/sway/lock.sh" \
   timeout 300 'swaymsg "output * dpms off"' \
-  timeout 600 '$HOME/.config/sway/suspend_if_battery.sh' \
   resume 'swaymsg "output * dpms on"' \
-  before-sleep "~/.config/sway/lock.sh" &
+  timeout 600 '$HOME/.config/sway/suspend_if_battery.sh' \
+  before-sleep "~/.config/sway/lock.sh" \
+  after-resume 'swaymsg "output * dpms on"' &
