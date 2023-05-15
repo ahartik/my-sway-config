e97bb0444a070aa80ff8b602e7f0a34b91f01b6a
Aleksi Hartikainen <ahartik@gmail.com>
2023-05-15 16:20:06 +0300

    laptop fixes

--- lock.sh
+++ lock.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Adapted in simpler form from
-# https://github.com/RedBorg/dotfiles-sway-island/blob/master/.config/swaylock/lock.sh
-
 original_dir="$(pwd)"
 
-mkdir -p /tmp/lock
-cd /tmp/lock
-grim -t jpeg screen.jpg
-rm -f blurred.jpg 
-ffmpeg -i screen.jpg -vf 'gblur=sigma=20' blurred.jpg
+$HOME/.config/sway/screenshot.sh
 
-swaylock --image blurred.jpg
+swaylock --image /tmp/lock/blurred.jpg
--- swayidle.sh
+++ swayidle.sh
@@ -2,7 +2,9 @@
 
 killall -w swayidle
 
-swayidle timeout 310 "~/.config/sway/lock.sh" \
+swayidle \
+  timeout 299 "~/.config/sway/screenshot.sh" \
+  timeout 310 "~/.config/sway/lock.sh" \
   timeout 300 'swaymsg "output * dpms off"' \
   resume 'swaymsg "output * dpms on"' \
   timeout 600 '$HOME/.config/sway/suspend_if_battery.sh' \
