22d23f6a1088bebe873fc0e3850565bb4bcfc12d
Aleksi Hartikainen <ahartik@gmail.com>
2024-09-03 17:41:43 +0300

    some bar formats

--- lock.sh
+++ lock.sh
@@ -4,4 +4,7 @@ original_dir="$(pwd)"
 
 $HOME/.config/sway/screenshot.sh
 
+# -p is for the swaylock-fprintd-git package
+# TODO: Debug why this doesn't work
+# swaylock -p --image /tmp/lock/blurred.jpg
 swaylock --image /tmp/lock/blurred.jpg
