ef64a64399895378ca7b2c06eb1eb0fcfad9f18e
Aleksi Hartikainen <ahartik@gmail.com>
2024-09-07 11:25:04 +0300

    locking image for multiple displays, corrupter instead of blur

--- lock.sh
+++ lock.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-original_dir="$(pwd)"
-
 $HOME/.config/sway/screenshot.sh
 
 # -p is for the swaylock-fprintd-git package
 # TODO: Debug why this doesn't work
 # swaylock -p --image /tmp/lock/blurred.jpg
-swaylock --image /tmp/lock/blurred.jpg
+LOCKARGS=
+
+# See screenshot.sh which takes these screenshots
+for OUTPUT in `swaymsg -t get_outputs | jq -r '.[].name'`
+do
+    BLURRED_IMAGE=/tmp/lock/$OUTPUT-blurred.jpg
+    LOCKARGS="${LOCKARGS} --image ${OUTPUT}:${BLURRED_IMAGE}"
+done
+
+swaylock $LOCKARGS
+
+
