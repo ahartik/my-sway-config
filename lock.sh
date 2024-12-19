f7f819deb975ddb6a1366e5e326b5375184eb7e5
Aleksi Hartikainen <ahartik@gmail.com>
2024-12-19 15:37:20 +0200

    many small things

--- lock.sh
+++ lock.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+if [[ $1 == "--screenshot" ]]
+then
 $HOME/.config/sway/screenshot.sh
+fi
+
+# PHYSICS TIME
+# swaylock --image $HOME/memes/second_quantization.png
+# exit 
 
 # -p is for the swaylock-fprintd-git package
 # TODO: Debug why this doesn't work
@@ -8,7 +15,7 @@ $HOME/.config/sway/screenshot.sh
 LOCKARGS=
 
 # See screenshot.sh which takes these screenshots
-for OUTPUT in `swaymsg -t get_outputs | jq -r '.[].name'`
+for OUTPUT in $(swaymsg -t get_outputs | jq -r '.[].name')
 do
     BLURRED_IMAGE=/tmp/lock/$OUTPUT-blurred.jpg
     LOCKARGS="${LOCKARGS} --image ${OUTPUT}:${BLURRED_IMAGE}"
--- swayidle.sh
+++ swayidle.sh
@@ -2,11 +2,11 @@
 
 killall -w swayidle
 
-swayidle \
+swayidle  \
   timeout 299 "~/.config/sway/screenshot.sh" \
   timeout 310 "~/.config/sway/lock.sh" \
   timeout 300 'swaymsg "output * dpms off"' \
   resume 'swaymsg "output * dpms on"' \
   timeout 600 '$HOME/.config/sway/suspend_if_battery.sh' \
-  before-sleep "~/.config/sway/lock.sh" \
+  before-sleep "~/.config/sway/lock.sh --screenshot" \
   after-resume 'swaymsg "output * dpms on"' &
